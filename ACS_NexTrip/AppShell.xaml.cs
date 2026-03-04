734495371072264e57c8f4d66564a88393012f65
stanislassz <stanislas.szylobryt@gmail.com>
2026-03-04 16:08:01 +0100

    Ajoutez des fichiers projet.

---
+++ ACS_NexTrip/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ACS_NexTrip
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ACS_NexTrip/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ACS_NexTrip")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ACS_NexTrip.Pages")]
