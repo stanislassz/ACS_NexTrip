92f9017f9fde5e658a764c0b5dfdbbe43b8679c7
szylobryt stanislas <stanislas.szylobryt@campus-la-chataigneraie.org>
2026-03-25 11:04:32 +0100

    Passage au format MVVM sur la page principale

--- ACS_NexTrip/ViewModel/HomeViewModel.cs
+++ ACS_NexTrip/ViewModel/HomeViewModel.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ACS_NexTrip.ViewModel
+﻿namespace ACS_NexTrip.ViewModel
 {
-    class HomeViewModel
+    using ACS_NexTrip.Pages;
+    using System.Threading.Tasks;
+    using System.Windows.Input;
+    using Microsoft.Maui.Controls;
+
+    public class HomeViewModel
     {
+        // Commandes pour les bindings du XAML
+        public ICommand NavigateToDashboardCommand { get; }
+        public ICommand NavigateToTripsCommand { get; }
+        public ICommand NavigateToUsersCommand { get; }
+        public ICommand ShowNotificationsCommand { get; }
+        public ICommand ShowProfileCommand { get; }
+
+        public HomeViewModel()
+        {
+            // Initialisation des commandes
+            NavigateToDashboardCommand = new Command(() => { /* Déjà sur l'accueil */ });
+            NavigateToTripsCommand = new Command(async () => await GoToTripsAsync());
+            NavigateToUsersCommand = new Command(async () => await GoToUsersAsync());
+            ShowNotificationsCommand = new Command(() => { /* À implémenter */ });
+            ShowProfileCommand = new Command(() => { /* À implémenter */ });
+        }
+
+        private async Task GoToTripsAsync()
+        {
+            //Dans un ViewModel, on utilise Shell ou Application.Current.MainPage pour naviguer
+            if (Application.Current?.MainPage != null)
+            {
+                await Application.Current.MainPage.Navigation.PushAsync(new TrajetPage());
+            }
+        }
+
+        private async Task GoToUsersAsync()
+        {
+            if (Application.Current?.MainPage != null)
+            {
+                await Application.Current.MainPage.Navigation.PushAsync(new SettingsPage());
+            }
+        }
     }
 }
