611f674bfeec3c1de5e84fd4da0a78441a065d36
szylobryt stanislas <stanislas.szylobryt@campus-la-chataigneraie.org>
2026-03-25 15:54:59 +0100

    Update de la page principale

--- ACS_NexTrip/ViewModel/HomeViewModel.cs
+++ ACS_NexTrip/ViewModel/HomeViewModel.cs
@@ -1,11 +1,16 @@
-﻿namespace ACS_NexTrip.ViewModel
-{
-    using ACS_NexTrip.Pages;
-    using System.Threading.Tasks;
-    using System.Windows.Input;
-    using Microsoft.Maui.Controls;
+﻿using ACS_NexTrip.Pages;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using ACS_NexTrip.Models;
+using ACS_NexTrip.Services;
+using CommunityToolkit.Mvvm.Input;
 
-    public class HomeViewModel
+namespace ACS_NexTrip.ViewModel
+{
+    public partial class HomeViewModel : ObservableObject
     {
         // Commandes pour les bindings du XAML
         public ICommand NavigateToDashboardCommand { get; }
@@ -32,6 +37,22 @@
         private async Task GoToUsersAsync()
         {
             await Shell.Current.GoToAsync("SettingsPage");
+        }
+
+        private readonly ConnexionBD _db;
+
+        [ObservableProperty]
+        private ObservableCollection<Trajet> _trajets;
+
+        public HomeViewModel(ConnexionBD db)
+        {
+            _db = db;
+            ChargerTrajets();
+        }
+
+        public async void ChargerTrajets()
+        {
+            Trajets = new ObservableCollection<Trajet>(await _db.GetNextTrajetsAsync());
         }
     }
 }
