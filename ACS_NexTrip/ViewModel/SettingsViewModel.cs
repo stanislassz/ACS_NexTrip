d57dc6a3fe9ee7577d46df8132df4cb86c1081f3
Thibs13 <caumontthibault1313@gmail.com>
2026-04-08 15:52:47 +0200

    Modification information paramètres

--- ACS_NexTrip/ViewModel/SettingsViewModel.cs
+++ ACS_NexTrip/ViewModel/SettingsViewModel.cs
@@ -5,15 +5,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
-using ACS_NexTrip.Services;
 using ACS_NexTrip.Models;
 using ACS_NexTrip.Services;
+using ACS_NexTrip.Services;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace ACS_NexTrip.ViewModel
 {
     public partial class SettingsViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private string _UTI_NOM;
+
+        [ObservableProperty]
+        private string _UTI_PRENOM;
+
+        [ObservableProperty]
+        private string _UTI_EMAIL;
+
+        [ObservableProperty]
+        private string _UTI_CP;
+
+        [ObservableProperty]
+        private string _UTI_TEL;
+
+        [ObservableProperty]
+        private string _UTI_ADRESSE;
+
+        [ObservableProperty]
+        private DateTime _DATE_NAISSANCE;
+
         // Commandes pour les bindings du XAML
         public ICommand NavigateToDashboardCommand { get; }
         public ICommand NavigateToTripsCommand { get; }
@@ -50,7 +72,6 @@ namespace ACS_NexTrip.ViewModel
         {
             await Shell.Current.GoToAsync("TrajetPage");
         }
-
         private async Task GoToSettingsAsync()
         {
             await Shell.Current.GoToAsync("SettingsPage");
@@ -58,6 +79,31 @@ namespace ACS_NexTrip.ViewModel
         private async Task GoToHomeAsync()
         {
             await Shell.Current.GoToAsync("HomePage");
+        }
+
+        [RelayCommand]
+        private async Task Save(Utilisateur utilisateur)
+        {
+            if (utilisateur == null) return;
+
+            try
+            {
+                // On appelle la méthode de mise à jour de ton service ConnexionBD
+                bool isSuccess = await _db.UpdateUtilisateurAsync(utilisateur);
+
+                if (isSuccess)
+                {
+                    await Shell.Current.DisplayAlert("Succès", "Vos informations ont été enregistrées.", "OK");
+                }
+                else
+                {
+                    await Shell.Current.DisplayAlert("Erreur", "Impossible de mettre à jour le profil.", "OK");
+                }
+            }
+            catch (Exception ex)
+            {
+                await Shell.Current.DisplayAlert("Erreur", ex.Message, "OK");
+            }
         }
     }
 }
