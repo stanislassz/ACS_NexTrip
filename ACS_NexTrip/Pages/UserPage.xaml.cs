b6a05d15f94250c579500270a83ff5f09485bc77
Thibs13 <caumontthibault1313@gmail.com>
2026-05-06 12:22:10 +0200

    update

--- ACS_NexTrip/Pages/UserPage.xaml.cs
+++ ACS_NexTrip/Pages/UserPage.xaml.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ACS_NexTrip.ViewModel;
 
 namespace ACS_NexTrip.Pages
 {
     public partial class UserPage : ContentPage
     {
-        public UserPage() 
+        public UserPage(UserViewModel vm) 
         {
             InitializeComponent();
+            BindingContext = vm;
         }
     }
 }
--- ACS_NexTrip/ViewModel/UserViewModel.cs
+++ ACS_NexTrip/ViewModel/UserViewModel.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ACS_NexTrip.Models;
+using ACS_NexTrip.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace ACS_NexTrip.ViewModel
 {
-    class UserViewModel
+    public partial class UserViewModel : ObservableObject
     {
+        private readonly ConnexionBD _db;
+
+        [ObservableProperty]
+        private ObservableCollection<Utilisateur> _utilisateurs;
+
+        public UserViewModel(ConnexionBD db)
+        {
+            _db = db;
+        }
+
+
+        [RelayCommand]
+        private async Task NavigateToDashboard() =>
+            await Shell.Current.GoToAsync("HomePage");
+
+        [RelayCommand]
+        private async Task NavigateToTrips() =>
+            await Shell.Current.GoToAsync("TrajetPage");
+
+        [RelayCommand]
+        private async Task NavigateToSettings() =>
+            await Shell.Current.GoToAsync("SettingsPage");
+
+        [RelayCommand]
+        private async Task NavigateToUsers() =>
+            await Shell.Current.GoToAsync("UsersPage");
+
+        [RelayCommand]
+        private async Task NavigateToLieu() =>
+            await Shell.Current.GoToAsync("LieuPage");
+
+        [RelayCommand]
+        private void ShowNotifications() { /* À implémenter */ }
+
+        [RelayCommand]
+        private void ShowProfile() { /* À implémenter */ }
     }
 }
