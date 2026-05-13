68e889580652b7c2e6505ef3b8366a5a24b0d41f
szylobryt stanislas <stanislas.szylobryt@campus-la-chataigneraie.org>
2026-05-13 11:15:13 +0200

    Revert "Refonte du header"
    This reverts commit a960d6970fcf07c8ca334215325f694fe5ebfe3c.

--- ACS_NexTrip/ViewModel/HomeViewModel.cs
+++ ACS_NexTrip/ViewModel/HomeViewModel.cs
@@ -44,5 +44,11 @@ namespace ACS_NexTrip.ViewModel
         [RelayCommand]
         private async Task NavigateToSettings() =>
             await Shell.Current.GoToAsync("SettingsPage");
+
+        [RelayCommand]
+        private void ShowNotifications() { /* À implémenter */ }
+
+        [RelayCommand]
+        private void ShowProfile() { /* À implémenter */ }
     }
 }
